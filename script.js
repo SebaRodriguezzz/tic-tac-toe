8316d3495d6dd96a9fc753ca3529520be8d2e98f
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-05-10 20:14:52 -0300

    Handle results of game in console

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const gameboard = (() => {
         this.appendChild(text);
         this.classList.add("mark");
         this.removeEventListener('click', placeMark);
+        checkWinner();
     }
 
     const addBoardListeners = (() => {
@@ -42,12 +43,50 @@ const gameboard = (() => {
         }
     })();
 
+    const addChildrenToArray = (array) => {
+        for (let row of table.rows){
+            for (let i = 0; i < row.cells.length; i++) {
+                array.push(row.cells[i].childNodes);
+            }
+        }
+    }
+
     const checkWinner = () => {
-        
+        let children = [];
+        addChildrenToArray(children);
+        if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[1][0]?.nodeValue && children[1][0]?.nodeValue === children[2][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[3][0]?.nodeValue != undefined && children[3][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[5][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[6][0]?.nodeValue != undefined && children[6][0]?.nodeValue === children[7][0]?.nodeValue && children[7][0]?.nodeValue === children[8][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[3][0]?.nodeValue && children[3][0]?.nodeValue === children[6][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[1][0]?.nodeValue != undefined && children[1][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[7][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[2][0]?.nodeValue != undefined && children[2][0]?.nodeValue === children[5][0]?.nodeValue && children[5][0]?.nodeValue === children[8][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[8][0]?.nodeValue){
+            console.log("winner");
+        } else if (children[2][0]?.nodeValue != undefined && children[2][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[6][0]?.nodeValue){
+            console.log("winner");
+        } else if(isTableFull(children)) {
+            console.log("tie");
+        }
+    }
+
+    const isTableFull = (arrayOfChildren) => {
+
+        for (let i = 0; i < arrayOfChildren.length; i++) {
+            if(arrayOfChildren[i][0] === undefined){
+                return false
+            }
+        }
+
+        return true;
     }
 
 
-    
     return {
         getContent
     }
