b05229b81903c209a305079b2b455021b791b2a0
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-05-12 22:12:11 -0300

    Improve row and column checking of function checkWinner

--- script.js
+++ script.js
@@ -54,17 +54,9 @@ const gameboard = (() => {
     const checkWinner = () => {
         let children = [];
         addChildrenToArray(children);
-        if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[1][0]?.nodeValue && children[1][0]?.nodeValue === children[2][0]?.nodeValue){
+        if (checkRow(children, 0) || checkRow(children, 3) || checkRow(children, 6)){
             console.log("winner");
-        } else if (children[3][0]?.nodeValue != undefined && children[3][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[5][0]?.nodeValue){
-            console.log("winner");
-        } else if (children[6][0]?.nodeValue != undefined && children[6][0]?.nodeValue === children[7][0]?.nodeValue && children[7][0]?.nodeValue === children[8][0]?.nodeValue){
-            console.log("winner");
-        } else if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[3][0]?.nodeValue && children[3][0]?.nodeValue === children[6][0]?.nodeValue){
-            console.log("winner");
-        } else if (children[1][0]?.nodeValue != undefined && children[1][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[7][0]?.nodeValue){
-            console.log("winner");
-        } else if (children[2][0]?.nodeValue != undefined && children[2][0]?.nodeValue === children[5][0]?.nodeValue && children[5][0]?.nodeValue === children[8][0]?.nodeValue){
+        } else if (checkColumn(children, 0) || checkColumn(children, 1) || checkColumn(children, 2)){
             console.log("winner");
         } else if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[8][0]?.nodeValue){
             console.log("winner");
@@ -75,15 +67,31 @@ const gameboard = (() => {
         }
     }
 
+    const checkRow = (children, n) => {
+        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + 1][0]?.nodeValue && children[n + 1][0]?.nodeValue === children[n + 2][0]?.nodeValue
+    }
+
+    const checkColumn = (children, n) => {
+        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + 3][0]?.nodeValue && children[n + 3][0]?.nodeValue === children[n + 6][0]?.nodeValue
+    }
+
+ 
+   
     const isTableFull = (arrayOfChildren) => {
 
-        for (let i = 0; i < arrayOfChildren.length; i++) {
+        let found = false;
+        let result = true;
+        let i = 0;
+
+        while(i < arrayOfChildren.length && found == false){
             if(arrayOfChildren[i][0] === undefined){
-                return false
+                found = true;
+                result = false;
             }
+            i++;
         }
 
-        return true;
+        return result;
     }
 
 
