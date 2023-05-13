8a50be92fab9cba4a5dc0daeda08f24b1e74963f
Sebastian Rodriguez <sebas.r1070@gmail.com>
2023-05-13 13:58:01 -0300

    Improve functions to check winner

--- script.js
+++ script.js
@@ -54,13 +54,7 @@ const gameboard = (() => {
     const checkWinner = () => {
         let children = [];
         addChildrenToArray(children);
-        if (checkRow(children, 0) || checkRow(children, 3) || checkRow(children, 6)){
-            console.log("winner");
-        } else if (checkColumn(children, 0) || checkColumn(children, 1) || checkColumn(children, 2)){
-            console.log("winner");
-        } else if (children[0][0]?.nodeValue != undefined && children[0][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[8][0]?.nodeValue){
-            console.log("winner");
-        } else if (children[2][0]?.nodeValue != undefined && children[2][0]?.nodeValue === children[4][0]?.nodeValue && children[4][0]?.nodeValue === children[6][0]?.nodeValue){
+        if (searchWinner(children)){
             console.log("winner");
         } else if(isTableFull(children)) {
             console.log("tie");
@@ -68,14 +62,44 @@ const gameboard = (() => {
     }
 
     const checkRow = (children, n) => {
-        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + 1][0]?.nodeValue && children[n + 1][0]?.nodeValue === children[n + 2][0]?.nodeValue
+        const multiplier = 1;
+        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + multiplier][0]?.nodeValue && children[n + multiplier][0]?.nodeValue === children[n + (multiplier * 2)][0]?.nodeValue
     }
 
     const checkColumn = (children, n) => {
-        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + 3][0]?.nodeValue && children[n + 3][0]?.nodeValue === children[n + 6][0]?.nodeValue
+        const multiplier = 3;
+        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + multiplier][0]?.nodeValue && children[n + multiplier][0]?.nodeValue === children[n + (multiplier * 2)][0]?.nodeValue
     }
 
- 
+    const checkDiagonal = (children, n) => {
+        let multiplier = 4;
+        if (n > 0){
+            multiplier = 2;
+        }
+        return children[n][0]?.nodeValue != undefined && children[n][0]?.nodeValue === children[n + multiplier][0]?.nodeValue && children[n + multiplier][0]?.nodeValue === children[n + (multiplier * 2)][0]?.nodeValue
+    }
+
+    const searchWinner = (arr) => {
+        
+        const searchLimit = 6;
+        let result = false;
+        let i = 0;
+
+        while (i <= searchLimit && result == false) {
+            if (i % 3 == 0){
+                result = checkRow(arr, i);
+            }
+            if (i <= 2 && result == false){
+                result = checkColumn(arr, i);
+            }
+            if (i % 2 == 0 && i <= 2 && result == false){
+                result = checkDiagonal(arr, i);
+            }
+            i++;
+        }
+
+        return result;
+    }
    
     const isTableFull = (arrayOfChildren) => {
 
